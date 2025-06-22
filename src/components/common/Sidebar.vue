54d6b28484665dc0b2a9aaa2740685060a600620
LJSoo <jisuu7813@naver.com>
2025-06-23 01:38:47 +0900

    Feat-ljs: 카테고리 강조 표시 기능

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -1,28 +1,37 @@
 <script setup>
+import { ref } from 'vue';
 import { useRouter, useRoute } from "vue-router";
+
 const router = useRouter();
 const route = useRoute();
 
 console.log(route);
 
+const selectedClickedItem = ref(null);
+
 const goHome = () => {
   router.push("/");
+  selectedClickedItem.value = 'home';
 };
 
 const goMyPage = () => {
   router.push("/mypage");
+  selectedClickedItem.value = 'myPage';
 };
 
 const goCommunity = () => {
   router.push("/community");
+  selectedClickedItem.value = 'community';
 };
 
 const goLogin = () => {
   router.push("/login");
+  selectedClickedItem.value = 'login';
 };
 
 const goSignup = () => {
   router.push("/signup");
+  selectedClickedItem.value = 'signup';
 };
 
 </script>
@@ -38,45 +47,55 @@ const goSignup = () => {
     <ul class="space-y-2 text-lg">
       <li
         class="cursor-pointer p-2 text-center"
-        :class="
-          route.name == 'Home' || route.name == 'BookDetail'
-            ? 'text-[#e3c02b] font-semibold'
-            : 'text-[#9baa59] hover:font-semibold'
-        "
+        :class="{
+          'text-[#e3c02b] font-semibold': route.name === 'Home' || route.name === 'BookDetail',
+          'text-[#9baa59] hover:font-semibold': !(route.name === 'Home' || route.name === 'BookDetail'), 
+          'bg-gray-200': selectedClickedItem === 'home'
+        }"
         @click="goHome"
       >
-        Home
+        홈
       </li>
       <li
         class="cursor-pointer p-2 text-center"
-        :class="
-          route.name == 'Community' || route.name == 'PostDetail'
-            ? 'text-[#e3c02b] font-semibold'
-            : 'text-[#9baa59] hover:font-semibold'
-        "
+        :class="{
+          'text-[#e3c02b] font-semibold': route.name === 'Community' || route.name === 'PostDetail',
+          'text-[#9baa59] hover:font-semibold': !(route.name === 'Community' || route.name === 'PostDetail'),
+          'bg-gray-200': selectedClickedItem === 'community'
+        }"
         @click="goCommunity"
       >
         Community
       </li>
       <li
         class="cursor-pointer p-2 text-center"
-        :class="
-          route.name == 'MyPage'
-            ? 'text-[#e3c02b] font-semibold'
-            : 'text-[#9baa59] hover:font-semibold'
-        "
+        :class="{
+          'text-[#e3c02b] font-semibold': route.name === 'MyPage',
+          'text-[#9baa59] hover:font-semibold': !(route.name === 'MyPage'),
+          'bg-gray-200': selectedClickedItem === 'myPage'
+        }"
         @click="goMyPage"
       >
         내 서재
       </li>
       <li
-        class="text-[#9baa59] hover:font-semibold cursor-pointer p-2 text-center"
+        class="cursor-pointer p-2 text-center"
+        :class="{
+          'text-[#e3c02b] font-semibold': route.name === 'Login',
+          'text-[#9baa59] hover:font-semibold': !(route.name === 'Login'),
+          'bg-gray-200': selectedClickedItem === 'login'
+        }"
         @click="goLogin"
       >
         로그인
       </li>
       <li
-        class="text-[#9baa59] hover:font-semibold cursor-pointer p-2 text-center"
+        class="cursor-pointer p-2 text-center"
+        :class="{
+          'text-[#e3c02b] font-semibold': route.name === 'Signup',
+          'text-[#9baa59] hover:font-semibold': !(route.name === 'Signup'),
+          'bg-gray-200': selectedClickedItem === 'signup'
+        }"
         @click="goSignup"
       >
         회원가입
